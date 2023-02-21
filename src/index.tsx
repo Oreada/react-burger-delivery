53064d61f3bcd9c60ca739e08c1b7785da8b1a36
Olga Antushevich <antushevich@gmail.com>
2023-02-21 15:15:09 +0200

    feat: add starting styles and normalize

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'normalize.css';
 import './index.css';
 import App from './App';
 
@@ -7,7 +8,5 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
