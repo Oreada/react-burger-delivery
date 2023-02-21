d3c0a01a75379a23ad87d93ba531e5f73c8550e5
Olga Antushevich <antushevich@gmail.com>
2023-02-21 16:35:19 +0200

    fix: fix the className

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,21 +1,22 @@
 import style from './Header.module.css';
 import logo from '../../img/logo.svg';
+import { Container } from '../Container/Container';
 
 export const Header = () => {
 	return (
-		<header className="header">
-			<div className="container header__container">
-				<img className="header__logo" src={logo} alt="Логотип YourMeal" />
+		<header className={style.header}>
+			<Container className={style.header__container}>
+				<img className={style.header__logo} src={logo} alt="Логотип YourMeal" />
 
-				<div className="header__wrapper">
-					<h1 className="header__title">
+				<div className={style.header__wrapper}>
+					<h1 className={style.header__title}>
 						<span>Только самые</span>
-						<span className="header__red">сочные бургеры!</span>
+						<span className={style.header__red}>сочные бургеры!</span>
 					</h1>
 
-					<p className="header__appeal">Бесплатная доставка от 599₽</p>
+					<p className={style.header__appeal}>Бесплатная доставка от 599₽</p>
 				</div>
-			</div>
+			</Container>
 		</header>
 	)
 };
